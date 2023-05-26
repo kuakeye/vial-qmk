ecb0682eb1397a6a21a9f7651afae10396c3a59f
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-05-26 15:52:22 -0400

    3x5_3_track in wall

--- keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
@@ -8,35 +8,60 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+// TRACKBALL IN WALL LAYOUT (STANDARD 3X5_3)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                      RAISE, KC_SPC, KC_BSPC,      LOWER,
-                                                                   KC_ENT,
-                                                                KC_LALT
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                            RAISE,   KC_SPC,  KC_TAB,      KC_LCTRL, KC_ENT, LOWER
+        ),
+    [_RAISE] = LAYOUT_split_3x5_3(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        S(KC_9), S(KC_0), KC_LCBR, KC_RCBR, KC_EQL,           KC_MINS, KC_4,    KC_5,    KC_6,    KC_QUOT,
+        QK_RBT,  QK_BOOT, KC_LBRC, KC_RBRC,_______,           _______, KC_1,    KC_2,    KC_3,    _______,
+                          _______,   _______,  QK_BOOT,      _______, _______, _______
         ),
 
     [_LOWER] = LAYOUT_split_3x5_3(
-        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
-        KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                     KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
-        _______,_______,_______,_______,SNIPING,                     _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL,
-                                      KC_BTN3, KC_BTN1,DRGSCRL,    _______,
-                                                                   _______,
-                                                                QK_BOOT
-
-        ),
-
-    [_RAISE] = LAYOUT_split_3x5_3(
-        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
-        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,
-        KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                     KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,
-                                        _______,_______,QK_BOOT,   _______,
-                                                                   _______,
-                                                                 _______
+        KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+        S(KC_9), S(KC_0), KC_LBRC,  KC_RBRC,KC_EQL,             _______,    KC_MINS, KC_EQL,  KC_PIPE, _______,
+        _______, _______, _______,  _______,_______,            _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+                          _______,   _______,  _______,      QK_BOOT, _______, _______
         )
 };
+
+// STANDARD 3-KEY TRACKBALL CLUSTER ON RIGHT
+//const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//    [_QWERTY] = LAYOUT_split_3x5_3(
+//        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+//        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+//        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+//                                      RAISE, KC_SPC, KC_BSPC,      LOWER,
+//                                                                   KC_ENT,
+//                                                                KC_LALT
+//        ),
+//
+//    [_LOWER] = LAYOUT_split_3x5_3(
+//        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
+//        KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                     KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
+//        _______,_______,_______,_______,SNIPING,                     _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL,
+//                                      KC_BTN3, KC_BTN1,DRGSCRL,    _______,
+//                                                                   _______,
+//                                                                QK_BOOT
+//
+//        ),
+//
+//    [_RAISE] = LAYOUT_split_3x5_3(
+//        KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
+//        _______,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,
+//        KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                     KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,
+//                                        _______,_______,QK_BOOT,   _______,
+//                                                                   _______,
+//                                                                 _______
+//        )
+//};
+
 
 #ifdef OLED_ENABLE
 
